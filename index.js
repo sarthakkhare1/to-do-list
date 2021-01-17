072ffd110e703a88ab78d29f4b4c38a38b9bc0c3
sarthak khare <kharesarthak09@gmail.com>
2021-01-17 23:06:55 +0530

    basic file structure

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
